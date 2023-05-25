677c5e42277856398418f35328056d29b180088a
Begoingto <begoingto.me@gmail.com>
2023-05-25 13:16:48 +0700

    metadata admin and loading🆗✅💯🎉

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import {desc} from "@/base/siteConfig";
+
+export const metadata = {
+    title: 'Dashboard',
+    description: desc,
+    openGraph:{
+        title: 'Dashboard',
+        description: desc,
+        images: [
+            {
+                url: "/og-image.jpg",
+                width: 800,
+                height: 600,
+            },
+        ]
+    },
+    twitter: {
+        title: {
+            template: 'Dashboard'
+        },
+        description: desc,
+        images: ["/og-image.jpg"],
+    }
+}
 
 function Dashboard(props) {
     return (
